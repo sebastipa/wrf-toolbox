dd8c748ca684c1020d5a1a73a27019d95b7a1ebf
Sebastiano Stipa <stipa@ealin43.private.vki.eu>
2024-12-02 13:05:37 +0100

    updated documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath('../../'))
 project = 'wrf-toolbox'
 copyright = '2024, Sebastiano Stipa'
 author = 'Sebastiano Stipa'
-release = '0.1'
+release = '0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
